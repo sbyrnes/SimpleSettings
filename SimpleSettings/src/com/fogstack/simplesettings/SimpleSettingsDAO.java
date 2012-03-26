ac62d9cecbb90217ab76cb6853e6f2a92cf4b547
sean <sean.byrnes@gmail.com>
2012-03-25 21:58:10 -0700

    Added the ability to update settings after created.

--- SimpleSettings/src/com/fogstack/simplesettings/SimpleSettingsDAO.java
+++ SimpleSettings/src/com/fogstack/simplesettings/SimpleSettingsDAO.java
@@ -8,6 +8,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+/** 
+ * Data access object for saving and retrieving settings data. 
+ * @author sean
+ *
+ */
 /*package*/ class SimpleSettingsDAO {
     private static final String DATABASE_NAME = "simplesettings.db";
     private static final int DATABASE_VERSION = 1;
@@ -55,21 +60,44 @@ import android.util.Log;
     
     public static SimpleSettingsDAO getInstance(Context ctx) { return new SimpleSettingsDAO(ctx); }
     
+    /** Open the database */
     private synchronized void open() throws SQLException {
     	Log.i("SimpleSettings", "Opening database");
         mDbHelper = new DatabaseHelper(mCtx);
         mDb = mDbHelper.getWritableDatabase();
     }
 
+    /** Close the database */
     public synchronized void close() {
     	Log.i("SimpleSettings", "Closing database");
         mDbHelper.close();
     }
     
+    /**
+     * Save an object identified by the given key.
+     * @param key
+     * @param value
+     */
     public void save(String key, Object value)
     {
     	open();
     	
+    	String currentValue = getString(key);
+    	if(currentValue == null)
+    	{
+    		create(key, value);
+    	} else {
+    		update(key, value);
+    	}
+    }
+    	
+    /** 
+     * Creates a setting with the given key and value. 
+     * @param key
+     * @param value
+     */
+    public void create(String key, Object value)
+    {
         try {
 			ContentValues initialValues = new ContentValues();
 			initialValues.put(SETTINGS_TABLE_KEY_KEY, key);
@@ -82,6 +110,31 @@ import android.util.Log;
 		}
     }
     
+    /**
+     * Updates the setting with the given key to have the given value.
+     * @param key
+     * @param value
+     */
+    private void update(String key, Object value)
+    {
+    	open();
+    	
+        try {
+			ContentValues initialValues = new ContentValues();
+			initialValues.put(SETTINGS_TABLE_KEY_VALUE, value.toString());
+			mDb.update(SETTINGS_TABLE_NAME, initialValues, SETTINGS_TABLE_KEY_KEY + "=?", new String[]{key});
+		} catch (Exception e) {
+			Log.e("SimpleSettings", "Unable to save setting", e);
+		} finally {
+	    	close();
+		}
+    }
+    
+    /**
+     * Returns the String value of the setting identified by key.
+     * @param key
+     * @return
+     */
     public String getString(String key)
     {
     	String result = null;
